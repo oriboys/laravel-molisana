ecbc5e923f87f4ce24b41143c0f4eefda1955992
andrea <corro.andrea.1995@gmail.com>
2021-03-10 17:27:19 +0100

    sezione prodotti

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,22 @@
 @extends('layouts.app')
+
+@section('content')
+<div class="products">
+  <div class="lista-prodotti">
+    @foreach ($pasta as $singolo)
+    <div class="prodotto">
+
+        <img src="{{$singolo['src']}}" alt="">
+
+        <a href="#">
+          <div class="hover-prodotto">
+
+            <p>{{$singolo['titolo']}} - {{$singolo['tipo']}}</p>
+          </div>
+
+         </a>
+    </div>
+    @endforeach
+  </div>
+</div>
+@endsection
