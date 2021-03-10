6c0e79c327a5351873cde04208573a7a097d3ccb
andrea <corro.andrea.1995@gmail.com>
2021-03-10 17:39:06 +0100

    news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,1 +1,14 @@
 @extends('layouts.app')
+
+
+@section('content')
+<div class="news">
+  <ul>
+    @foreach ($pasta as $singolo)
+      <li>La nuova pasta {{$singolo['titolo']}} è ora disponibile</li>
+          <img src="{{$singolo['src']}}" alt="">
+
+    @endforeach
+    </ul>
+</div>
+@endsection
