23caa092b112486747834a50e83d321d6a35baba
oriboys <corro.andrea.1995@gmail.com>
2021-03-11 17:08:31 +0100

    pagina prodotto

--- resources/views/description.blade.php
+++ resources/views/description.blade.php
@@ -1,8 +1,24 @@
-@extend('layout.app')
+@extends('layouts.app')
 
 @section('content')
-<div class="">
-  <h1>ciao</h1>
+<div class="detailes">
+  <h1>{{$prodotto['titolo']}}</h1>
+  <div class="immagini">
+    <img src="{{$prodotto['src-h']}}" alt="">
+    <img src="{{$prodotto['src-p']}}" alt="">
+  </div>
+  <div class="info">
+      <ul>
+        <li><i class="fas fa-info"></i>  {{$prodotto['tipo']}} </li>
+        <li><i class="fas fa-clock"></i> {{$prodotto['cottura']}} </li>
+        <li><i class="fas fa-weight"></i> {{$prodotto['peso']}} </li>
+      </ul>
+  </div>
+
+<div class="desc">
+  {!!$prodotto['descrizione']!!}
+</div>
+
 
 </div>
 @endsection
